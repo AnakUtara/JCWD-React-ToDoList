f5f6de4fa6d4508ffadbf262f0a624db0db0d6ab
AnakUtara <tok.riady@gmail.com>
2024-03-30 21:39:12 +0700

    fix logo responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@
 export default function Header({ logo, children }) {
 	return (
 		<div className="flex items-center justify-between gap-0 w-full mb-5">
-			<h2 className="w-fit font-gaba font-black text-3xl uppercase min-[320px]:text-2xl">
+			<h2 className="w-fit font-gaba font-black min-[420px]:text-3xl sm:text-4xl uppercase min-[320px]:text-2xl">
 				{logo}
 			</h2>
 			<div className="divider divider-horizontal max-[320px]:ml-0 before:bg-cyan-400 after:bg-cyan-400"></div>
